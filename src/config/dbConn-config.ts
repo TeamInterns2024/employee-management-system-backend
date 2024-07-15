cbe3636a35dd3c80820cc82c251e4ab327785b72
nishanth-s03 <22ca060.nishanth@sjec.ac.in>
2024-07-15 10:48:28 +0530

    Created Connection Between MongoDB and Express

--- src/config/dbConn-config.ts
+++ src/config/dbConn-config.ts
@@ -1,5 +1,22 @@
-//Connection Page
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
 
-console.log('====================================');
-console.log('Connect');
-console.log('====================================');
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+const connectDB = async () => {
+    const uri: string = process.env.CONNECTION_URI || 'Null';
+    console.log('[Database]: Connecting to MongoDB with URI:', uri);  
+
+    if (!uri.startsWith('mongodb://') && !uri.startsWith('mongodb+srv://')) {
+        console.error('[Database]: Invalid connection string:', uri);
+        return;
+    }
+
+    mongoose.connect(uri)
+        .then(() => console.log('[Database]: Connected To Database'))
+        .catch(error => console.error('[Database]: Error connecting to MongoDB:', error.message));
+};
+
+
+export default connectDB;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/dbConn-config";
 
 dotenv.config();
 
@@ -10,6 +11,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+//Connecting Database and Server
+connectDB().
+then(() =>{ 
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  });
+}).
+catch(error => console.error('[Server]: Error Occured',error.message));
